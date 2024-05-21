ab7b540dedac3e3eeea705c8478e076a91a4b9e3
dw1a <davidSimal>
2024-05-21 13:12:08 +0200

    Validacion inicial

---
+++ proyecto_git_DAVID_SIMAL/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dw1a
+ *
+ */
+module proyecto_git_DAVID_SIMAL {
+}
---
+++ proyecto_git_DAVID_SIMAL/src/proyecto_git_DAVID_SIMAL/ClaseRamaMaster.java
@@ -0,0 +1,9 @@
+package proyecto_git_DAVID_SIMAL;
+
+public class ClaseRamaMaster {
+	
+	public static void main(String[] args ) {
+		System.out.println("Primer fichero del paquete git en la rama Master");
+	}
+
+}
